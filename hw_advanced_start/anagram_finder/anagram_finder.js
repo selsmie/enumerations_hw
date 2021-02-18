838bcacc6e57759485af492e459512fa64f92c89
selsmie <simonelsmie@hotmail.com>
2021-02-18 10:39:03 +0000

    removed commenting

--- hw_advanced_start/anagram_finder/anagram_finder.js
+++ hw_advanced_start/anagram_finder/anagram_finder.js
@@ -3,14 +3,27 @@ const AnagramFinder = function (word) {
     this.split_word = this.word.toLowerCase().split("")
 }
 
+// AnagramFinder.prototype.findAnagrams = function (otherWords) {
+//     //iterate through otherWords
+//     return otherWords.filter((otherWord) => {
+//         //convert otherWord to lower case and split
+//         let check_word = otherWord.toLowerCase().split("")
+//         // if check_word and split_word are the same length then good
+//         if (check_word.length === this.split_word.length) {
+//             // if otherWord is not this.word
+//             if (this.word !== otherWord) {
+//                 return check_word.every((letter) => {
+//                     return this.split_word.includes(letter)
+//                 })
+//             }
+//         }
+//     })
+// }
+
 AnagramFinder.prototype.findAnagrams = function (otherWords) {
-    //iterate through otherWords
     return otherWords.filter((otherWord) => {
-        //convert otherWord to lower case and split
         let check_word = otherWord.toLowerCase().split("")
-        // if check_word and split_word are the same length then good
         if (check_word.length === this.split_word.length) {
-            // if otherWord is not this.word
             if (this.word !== otherWord) {
                 return check_word.every((letter) => {
                     return this.split_word.includes(letter)
